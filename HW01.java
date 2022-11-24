c0dddfad478efbdd0f433a329408599acd6b3fc1
Igor Bondarev <garkol@bk.ru>
2022-11-25 00:46:28 +0300

    Java HomeWork01 correction

--- HW01.java
+++ HW01.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 import java.util.Random;
 
 public class HW01 {
@@ -13,16 +13,15 @@ public class HW01 {
     public static void randInt() {
         //1. Выбросить случайное целое число в диапазоне от -1000 до 1000 и сохранить в i
 
-        int ri = new Random().nextInt(-1000, 1001);
-        i = ri;
+        i = new Random().nextInt(-1000, 1001);
+
         System.out.println("Случайно выпавшее число: " + i);
     }
 
     public static void maxByte(int num) {
         //2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
 
-        int number = 0;
-        number = Integer.highestOneBit(Math.abs(num));
+        int number = Integer.highestOneBit(Math.abs(num));
 
         while (number/2 > 0) {
             number = number/2;
@@ -70,21 +69,21 @@ public class HW01 {
                 count_notdivisible ++;
             }
         }
-        int m1[] = new int[count_notdivisible];
+        int m2[] = new int[count_notdivisible];
         int count = 0;
         for (int k = Short.MIN_VALUE; k < i; k++) {
             if (k % n != 0) {
-                m1[count] = k;
+                m2[count] = k;
                 count++;
             }
         }
         System.out.print("Числа НЕ кратные старшему значащему биту: \n[");
-        for (int l = 1; l < m1.length ; l++) {
-            if (l == m1.length - 1) {
-                System.out.println(m1[l - 1] + "]");
+        for (int l = 1; l < m2.length ; l++) {
+            if (l == m2.length - 1) {
+                System.out.println(m2[l - 1] + "]");
                 break;
             }
-            System.out.print(m1[l - 1] + ", ");
+            System.out.print(m2[l - 1] + ", ");
             if (l % 25 == 0) {
                 System.out.println();
             }
